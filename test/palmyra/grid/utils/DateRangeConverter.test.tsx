08b6af1153b29f0ff69b55b5faeff42cf8d9f77d
surya-ponmani <gsuryaprakash2000@gmail.com>
2024-09-27 15:06:14 +0530

    DateRange Test case add

--- test/palmyra/grid/utils/DateRangeConverter.test.tsx
+++ test/palmyra/grid/utils/DateRangeConverter.test.tsx
@@ -8,7 +8,10 @@ describe("DateRangeConverter", () => {
         const props: IPattern = {};
         const converter = new DateRangeConverter(props, "DD-MM-YYYY");
 
-        const dateRange: IRange<any> = {}
+        const dateRange: IRange<any> = {
+            from: '',
+            to: ''
+        }
 
         expect(() => converter.format(dateRange)).toBeUndefined;
     });
@@ -25,6 +28,121 @@ describe("DateRangeConverter", () => {
         const expected = '01-10-2024...05-10-2024';
         const actual = converter.format(dateRange);
         expect(actual).toEqual(expected);
+    });
+
+    test('DateRangePicker Convert with Pattern Mismatch', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "DD-MM-YYYY");
+
+        const dateRange: IRange<any> = {
+            from: '10-01-2024',
+            to: '10-05-2024'
+        };
+
+        expect(() => converter.format(dateRange)).toBeUndefined;
+    });
+
+    test('DateRangePicker Convert with Invalid Date Format', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "YYYY-MM-DD");
+
+        const dateRange: IRange<any> = {
+            from: '2024-01-10',
+            to: '2024-05-10'
+        };
+
+        expect(() => converter.format(dateRange)).toBeUndefined;
+    });
+
+    test('DateRangePicker Convert with Reversed from and to Dates', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "MM-DD-YYYY");
+
+        const dateRange: IRange<any> = {
+            from: '05-10-2024',
+            to: '01-10-2024' 
+        };
+
+        const expected = '05-10-2024...01-10-2024';
+        const actual = converter.format(dateRange);
+        expect(actual).toEqual(expected);
+    });
+
+    test('DateRangePicker Convert with Partially Invalid Date Range', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "MM-DD-YYYY");
+
+        const dateRange: IRange<any> = {
+            from: '01-10-2024',
+            to: '10-2024'
+        };
+
+        expect(() => converter.format(dateRange)).toBeUndefined;
+    });
+
+    test('DateRangePicker Convert with Same from and to Date', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "MM-DD-YYYY");
+
+        const dateRange: IRange<any> = {
+            from: '01-10-2024',
+            to: '01-10-2024'
+        };
+
+        const formattedRange = converter.format(dateRange);
+        const expected = '01-10-2024...01-10-2024';
+        expect(formattedRange).toEqual(expected);
+    });
+
+    test('DateRangePicker Convert with Invalid Date Components', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "MM-DD-YYYY");
+
+        const dateRange: IRange<any> = {
+            from: '13-32-2024',
+            to: '02-30-2024'
+        };
+
+        expect(() => converter.format(dateRange)).toBeUndefined;
+    });
+
+    test('DateRangePicker Convert with Null or Undefined Dates', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "MM-DD-YYYY");
+    
+        const dateRangeWithNull: IRange<any> = {
+            from: null,
+            to: '01-10-2024'
+        };
+    
+        const dateRangeWithUndefined: IRange<any> = {
+            from: '01-10-2024',
+            to: undefined
+        };
+    
+        const fromExpected = '>01-10-2024';
+        const fromActual = converter.format(dateRangeWithUndefined);
+
+        const toExpected = '<01-10-2024';
+        const toActual = converter.format(dateRangeWithNull);
+
+        expect(fromActual).toEqual(fromExpected);
+        expect(toActual).toEqual(toExpected);
+    });
+
+    test('DateRangePicker Convert with Time Included in Date', () => {
+        const props: IPattern = { serverPattern: 'MM-DD-YYYY' };
+        const converter = new DateRangeConverter(props, "MM-DD-YYYY");
+    
+        const dateRange: IRange<any> = {
+            from: '01-10-2024 10:30 AM',  
+            to: '05-10-2024 5:00 PM'
+        };
+    
+        const formattedRange = converter.format(dateRange);
+    
+        const expected = '01-10-2024...05-10-2024';
+        expect(formattedRange).toEqual(expected);
     });
 
     test('DateRangePicker Convert FromDate', () => {
