4a269f508dd5ff421d095b45b5177ab472d61b07
Raja K <ksvraja@gmail.com>
2024-10-11 17:07:13 +0530

    server lookup test - mocking palmyrastore factory

--- test/palmyra/form/hooks/useSeverlookupFieldManager/Serverlookup.test.tsx
+++ test/palmyra/form/hooks/useSeverlookupFieldManager/Serverlookup.test.tsx
@@ -1,33 +1,9 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useServerLookupFieldManager, PalmyraForm } from "../../../../../src/palmyra";
 import { IServerLookupDefinition } from "../../../../../demo/palmyra/mui/form/types";
 import { PalmyraStoreFactory } from "@palmyralabs/palmyra-wire";
-import { vi } from 'vitest';
-import orgAxios from "axios";
+import { beforeAll, describe, test, vi } from 'vitest';
 
-let axios = vi.mocked(orgAxios, true)
-
-beforeAll(() => {
-    vi.resetAllMocks();
-    axios = vi.mocked(orgAxios, true)
-});
-
-const mockFetch = (data) => {
-    global.fetch = vi.fn(() =>
-        Promise.resolve({
-            ok: true,
-            status: 200,
-            statusText: 'OK',
-            headers: new Headers(),
-            redirected: false,
-            type: 'basic',
-            url: '/masterdata',
-            json: () => Promise.resolve(data),
-        } as Response)
-    );
-};
-
-const storeFactory = new PalmyraStoreFactory({ baseUrl: '/api/palmyra' })
 
 describe('useServerLookupFieldManager', () => {
     const data = [
@@ -36,103 +12,40 @@ describe('useServerLookupFieldManager', () => {
         { id: 3, name: "Location" }
     ]
 
-    test('get data', async () => {
-
-        mockFetch(data);
-        const wrapper = ({ children }) => {
-            return <PalmyraForm formData={{ lookup: { id: 1, name: "Name" } }} storeFactory={storeFactory}>{children} </PalmyraForm>
-        }
-        const options: IServerLookupDefinition = {
-            queryOptions: { endPoint: '/masterdata' },
-            attribute: 'lookup'
-        }
-        const { result } = renderHook(() => useServerLookupFieldManager('lookup', options), { wrapper })
-
-        expect(result.current.options).toStrictEqual([{ id: 1, name: "Name" }]);
-        expect(result.current.isValid()).toBeTruthy();
-        expect(result.current.getValue()).toStrictEqual({ id: 1, name: "Name" });
+    beforeAll(() => {
+        vi.resetAllMocks();
     });
 
-    test('default data', async () => {
+    test('get data', async () => {
+        const storeFactory = new PalmyraStoreFactory({ baseUrl: '/api/palmyra' })
+        const lookupStore: any = {
+            query() {
+                return Promise.resolve({ result: data, total: data.length })
+            }
+        };
 
-        // mockFetch(data);
+        vi.spyOn(storeFactory, "getLookupStore").mockReturnValue(lookupStore)
+
         const wrapper = ({ children }) => {
             return <PalmyraForm formData={{}} storeFactory={storeFactory}>{children} </PalmyraForm>
         }
+
         const options: IServerLookupDefinition = {
-            queryOptions: { endPoint: '/masterdata' },
-            attribute: 'lookup',
-            defaultValue: { id: 3, name: "Location" }
+            queryOptions: { endPoint: 'masterdata' },
+            attribute: 'lookup'
         }
-        const { result } = renderHook(() => useServerLookupFieldManager('lookup', options), { wrapper })
 
-        expect(result.current.options).toStrictEqual([{ id: 3, name: "Location" }]);
-        expect(result.current.isValid()).toBeTruthy();
-        expect(result.current.getValue()).toStrictEqual({ id: 3, name: "Location" });
-    });
+        const { result } = renderHook(() => useServerLookupFieldManager('lookup', options), { wrapper });
 
-    test('get data', async () => {
+        act(() => {
+            result.current.refreshOptions();
+        })
 
-        const wrapper = ({ children }) => {
-            return <PalmyraForm formData={{ lookup: { id: 1, name: "Name" } }} storeFactory={storeFactory}>{children} </PalmyraForm>
-        }
-        const options: IServerLookupDefinition = {
-            queryOptions: { endPoint: '/masterdata' },
-            attribute: 'lookup.id'
-        }
-        const { result } = renderHook(() => useServerLookupFieldManager('lookup', options), { wrapper })
+        console.log(result.current.options);
 
-        expect(result.current.isValid()).toBeTruthy();
-        expect(result.current.getValue()).toStrictEqual({ id: 1, name: "Name" });
-    });
-
-    // test('get data', async () => {
-    //     const asd = axios.get.mockResolvedValue({ data: { result: data } });
-    //     const actualres = await asd
-
-    //     const wrapper = ({ children }) => {
-    //         return <PalmyraForm formData={{ lookup: 1 }} storeFactory={storeFactory}>{children} </PalmyraForm>
-    //     }
-    //     const options: IServerLookupDefinition = {
-    //         queryOptions: { endPoint: '/masterdata' },
-    //         attribute: 'lookup'
-    //     }
-    //     const { result } = renderHook(() => useServerLookupFieldManager('lookup', options), { wrapper })
-    //     result.current.options = data;
-
-    //     console.log(actualres, result.current.options)
-
-    //     expect(result.current.options).toStrictEqual([
-    //         { id: 1, name: 'Name' },
-    //         { id: 2, name: 'Date' },
-    //         { id: 3, name: 'Location' }
-    //     ]);
-
-    //     expect(actualres).toStrictEqual(result.current.options);
-    //     expect(result.current.isValid()).toBeTruthy();
-    //     expect(result.current.getValue()).toBe(1);
-    // });
-
-
-
-    test('get data', async () => {
-        const wrapper = ({ children }) => {
-            return <PalmyraForm formData={{ lookup: { id: 1, name: "Name" } }} storeFactory={storeFactory}>{children} </PalmyraForm>
-        }
-        const options: IServerLookupDefinition = {
-            queryOptions: { endPoint: '/masterdata' },
-            attribute: 'lookup.id'
-        }
-        const { result } = renderHook(() => useServerLookupFieldManager('lookup', options), { wrapper })
-        result.current.options = data;
-
-        expect(result.current.options).toStrictEqual([
-            { id: 1, name: 'Name' },
-            { id: 2, name: 'Date' },
-            { id: 3, name: 'Location' }
-        ]);
-        expect(result.current.isValid()).toBeTruthy();
-        expect(result.current.getValue()).toStrictEqual({ id: 1, name: "Name" });
+        // expect(result.current.options).toStrictEqual([{ id: 1, name: "Name" }]);
+        // expect(result.current.isValid()).toBeTruthy();
+        // expect(result.current.getValue()).toStrictEqual({ id: 1, name: "Name" });
     });
 
 })
