e2eded512864a15fb489573c4da4a962f15ca0f5
surya-ponmani <gsuryaprakash2000@gmail.com>
2024-11-15 10:55:59 +0530

    acl page set

--- demo/palmyra/apiControl/ApiAccessMgmt.tsx
+++ demo/palmyra/apiControl/ApiAccessMgmt.tsx
@@ -2,17 +2,19 @@
 import { AclAPIEditor } from '../../../src/palmyra/acl/AclAPIEditor';
 const response = [
     {
+        name: "DELETE",
         code: "DELETE",
         operations: "Delete",
         mask: 1
     },
     {
+        name: "QCRU",
         code: "QCRU",
         operations: "All Crud, Search",
         mask: 0
     }
 ];
-const apiList = Array.from({ length: 78 }, (_, index) => {
+const apiList = Array.from({ length: 80 }, (_, index) => {
     const item = response[index % response.length];
     const pageNumber = Math.floor(index / 2) + 1;
     return {
@@ -29,6 +31,7 @@ const ApidataessMgmt = () => {
             data[item.className] = [];
         }
         data[item.className].push({
+            name: item.name,
             code: item.code,
             operations: item.operations,
             mask: item.mask
@@ -40,7 +43,6 @@ const ApidataessMgmt = () => {
         className,
         permissions
     }));
-
     return (
         <AclAPIEditor data={modifyApiList} />
     )
