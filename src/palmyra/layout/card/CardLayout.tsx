260d513bfe7cff7be755164e20c0eecc8bbc5776
Raja K <ksvraja@gmail.com>
2024-12-28 15:23:02 +0530

    ChildKeyProvider to be optional parameter

--- src/palmyra/layout/card/CardLayout.tsx
+++ src/palmyra/layout/card/CardLayout.tsx
@@ -8,19 +8,20 @@ interface CardLayoutInput {
     Child: React.FC,
     EmptyChild?: React.FC
     Loading?: React.FC
-    childProps: any,
+    childProps?: any,
     title?: string,
-    childKeyProvider: (data: any, index: number) => string | number,
+    childKeyProvider?: (data: any, index: number) => string | number,
     preProcess?: (data: any, index: number) => any
 }
 
 const CardLayout = (props: CardLayoutInput) => {
-    const { children, dataList, Child, childProps } = props;
+    const { children, dataList, Child } = props;
     if (!dataList) {
         const Loading = props.Loading || NoopEmptyChildCard;
         return <Loading />
     }
 
+    const childProps = props.childProps || {};
     const childKeyProvider = props.childKeyProvider || ((data: any, index: number) => index);
     const preProcess = props.preProcess || ((d: any, index?: number) => d);
     const EmptyChild = props.EmptyChild || EmptyChildCard;
