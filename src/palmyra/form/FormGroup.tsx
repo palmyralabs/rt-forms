1f2a55dfeddb6928208592abeaa77a3a68f62761
surya-ponmani <surya@palmyralabs.com>
2025-12-09 14:12:11 +0530

    formgroup headercontent added

--- src/palmyra/form/FormGroup.tsx
+++ src/palmyra/form/FormGroup.tsx
@@ -1,13 +1,17 @@
 import './FormGroup.css';
 interface FormGroupProps {
     title?: string;
+    headerContent?: any;
     children: React.ReactNode;
 }
 
-const FormGroup: React.FC<FormGroupProps> = ({ title, children }) => {
+const FormGroup: React.FC<FormGroupProps> = ({ title, children, headerContent }) => {
     return (
         <div className="py-form-group">
-            <div className="py-form-group-title">{title}</div>
+            <div className="py-form-group-title">
+                {title}
+                {headerContent && <div>{headerContent}</div>}
+            </div>
             {children}
         </div>
     );
